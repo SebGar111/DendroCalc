7e5fd888cd33be68994a5916daae8b81fa0927b4
Sebastian Garlacz <sebastiangarlacz111@gmail>
2023-04-12 01:56:49 +0200

    change fallowing files

---
+++ Wyniki pomiaru sekcyjnego.py
@@ -0,0 +1,5 @@
+#code UTF=8
+mi�szo��_sekcji_1_to_0.212_m3
+mi�szo��_sekcji_2_to_0.198_m3
+mi�szo��_sekcji_1_to_0.198_m3
+mi�szo��_sekcji_2_to_0.185_m3
--- pomiar sekcyjny drzewa leżacego.py
+++ pomiar sekcyjny drzewa leżacego.py
@@ -18,7 +18,7 @@ print(numer_of_complite_sec)
 
 mid_of_sec=lenght_of_sec/2
 mid_of_sec=int(mid_of_sec)
-
+summaryvolume=0
 b=0+mid_of_sec
 #sekcje drzewa
 cyfra=0
@@ -28,7 +28,14 @@ while  b  <leght_of_arrow_without_arrowhead:
         x = (x ** 2 * pi) / 40000 * lenght_of_sec
         x=round(x,3)
         cyfra += 1
+        summaryvolume += x
+        pomiar=open("Wyniki pomiaru sekcyjnego.py","a+")
+        x=str(x)
+        pomiar.write(f"miąszość_sekcji_{cyfra}_to_{x}_m3\n")
+        pomiar.close()
         print(f"miąszość sekcji {cyfra} wynosi {x}m3")
+        print(f"suma miaszosci skecji{round(summaryvolume,3)}")
+
         b+=lenght_of_sec
 
 
